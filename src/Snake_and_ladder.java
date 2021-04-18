3dc9798062e3d2e9f1add44cba0b1cef7001c9e7
Abhishek <ABHISHEKKUMAR998>
2021-04-18 17:14:47 +0530

    Done with all UC

--- src/Snake_and_ladder.java
+++ src/Snake_and_ladder.java
@@ -24,37 +24,30 @@ public class Snake_and_ladder {
 		    board[56] = 24;
 		    board[49] = 17;
 		 
-	  while(pos<=100) {
+	  while(pos<100) {
 		       int dice= min +(int)(Math.random()*((max-min)+1));
-		                      if(pos<=100) {
-		    	                  pos+=dice;
+		                      if(pos+dice>100) {
+		    	                  c++;
+		    	                  continue;
                                      }		       
-		                         c++;
-		                         
-		                         if(pos>100) {
-		                        	  temp=pos;
-		                        	 temp=temp-dice;
-		                         }
-		                         //System.out.println(temp);
-		                         
-		                         if(pos<=100)
-		                        System.out.println(pos);
+		                          pos+=dice;
+		                  
 		                          if(pos==2||pos==4||pos==10||pos==19||pos==26||pos==40||pos==75||pos==90) {
 			                          pos= board[pos];
-			                          System.out.println(pos);
-			                          
+			                        
 		                            }
 		                         else if (pos==22||pos==43||pos==99||pos==29||pos==56||pos==49) {
 			                          pos= board[pos];
-			                          System.out.println(pos);
 		                            }
 		                        if(pos==100) {
+		                        	System.out.println(pos);
 			                   System.out.println("Player win the game"+" and he have to rolled the dice  "+c);
+			                   break;
 		
 	                           }
-		                
-	  
-	                 }
+		                        c++;
+		                        System.out.println(pos);
+	  							}
 
 }
 }
